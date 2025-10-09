636a9e86f39e8dcea2a0fb5626fa8d986caf0bb4
jefersonguedes89 <jefersonguedes89@gmail.com>
2025-10-09 12:28:51 -0300

    create app top menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegisterComponent } from "./pages/register/register.component";
+import { TopMenuComponent } from "./shared/components/global/top-menu/top-menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterComponent],
+  imports: [RouterOutlet, TopMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
