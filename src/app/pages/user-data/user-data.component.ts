dce63c96bdecacae4627fe004cf94bdf0ef0ca44
jefersonguedes89 <jefersonguedes89@gmail.com>
2025-10-15 12:31:30 -0300

    create user data component

---
+++ src/app/pages/user-data/user-data.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { UserResponse, UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-user-data',
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './user-data.component.html',
+  styleUrl: './user-data.component.scss',
+})
+export class UserDataComponent {
+  // form!: FormGroup;
+  // user: UserResponse | null;
+
+  // constructor(
+  //   private formBuilder: FormBuilder,
+  //   private userService: UserService
+  // )
+  // {
+  //   this.user = this.userService.getUser();
+  //   this.form = this.formBuilder.group(
+  //     {
+  //       nome: [{value: this.user?.nome || '', disabled:true}],
+  //       email:[{value: this.user?.email || '', disabled:true}],
+  //     }
+  //   )
+  // }
+
+  private formBuilder = inject(FormBuilder);
+  private userService = inject(UserService);
+  
+  user = this.userService.getUser();
+  form = this.formBuilder.group({
+    nome: [{ value: this.user?.nome || '', disabled: true }],
+    email: [{ value: this.user?.email || '', disabled: true }],
+  });
+}
